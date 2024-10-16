78fb14d67a0aa18c6c10c6b3bd8c7fda63e59cee
AisTattoo <adilmubarak126@gmail.com>
2024-10-16 09:45:02 +0700

    first commit

---
+++ Tugas.py
@@ -0,0 +1,14 @@
+class Mobil:
+  name = ""
+  pintu = 0
+  kursi = 0
+  roda = 0
+  spion = 0
+mobilinnova = Mobil()
+print(mobilinnova.name)
+mobilinnova.name = "Innova"
+mobilinnova.pintu = 4
+mobilinnova.kursi = 5
+mobilinnova.roda = 4
+mobilinnova.spion = 2
+print(f"name: {mobilinnova.name}, pintu: {mobilinnova.pintu}, kursi: {mobilinnova.kursi}, roda: {mobilinnova.roda}, spion: {mobilinnova.spion}")
